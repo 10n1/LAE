a0aea8f3d5dd7432ed469e2588c58361ac5baf71
Kyle Weicht <kyle@awesomekyle.com>
2012-06-06 23:38:37 -0700

    Adding super basic core and tests

--- src/core/core.c
+++ src/core/core.c
@@ -1,33 +1,51 @@
 /*
- *  test_core.cpp
+ *  core.c
  *  lae
  *
  *  Created by Kyle C Weicht on 6/6/12.
  *  Copyright (c) 2012 Kyle C Weicht. All rights reserved.
  */
+#include "lae_core.h"
 
 /*----------------------------------------------------------------------------*/
 /* C headers */
-/* C++ headers */
+#include <stdlib.h>
+#include <string.h>
 /* External headers */
 /* Internal headers */
-#include "lae_test.h"
+#include "lae_timer.h"
 /*----------------------------------------------------------------------------*/
 
-namespace
-{
 /*----------------------------------------------------------------------------*\
 Internal
 \*----------------------------------------------------------------------------*/
-TEST(CreateAndDestroyCore)
+struct lae_core
 {
-    lae_core* core = lae_core_create();
-    CHECK_POINTER_NOT_NULL(core);
-    lae_core_destroy(core);
-}
-
-} // anonymous namespace
+    lae_timer_t main_timer;
+};
 
 /*----------------------------------------------------------------------------*\
 External
 \*----------------------------------------------------------------------------*/
+lae_core* lae_core_create(void)
+{
+    lae_core* core = NULL;
+    
+    /* Allocate the core */
+    core = (lae_core*)malloc(sizeof(*core));
+    memset(core, 0, sizeof(*core));
+    
+    /* Set up subsystems */
+    
+    /* Get the ball rolling */
+    lae_timer_init(&core->main_timer);
+    
+    return core;
+}
+void lae_core_destroy(lae_core* core)
+{
+    /* Shutdown subsystems */
+    
+    /* Deallocate the core */
+    free(core);
+}
